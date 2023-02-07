9e552895a97488c0b37f5633b4c0f3ea1207b7d5
Petya Gavrilova <petya.gavrilova@hashicorp.com>
2023-02-07 16:10:10 +0200

    main.tf commited

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "random" {
+  
+}
+
+resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name on each apply
+   
+ }
+ 
+}
+
+/*
+variable "midcheck"{
+    default = "testname"
+}
+*/
+
+output "value" {
+    value = "MyNewPetTypeIs-${random_pet.server.id}"
+
+}
